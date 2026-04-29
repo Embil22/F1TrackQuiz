10536376ba2b3e87c1d4c4ada95037dcdfb0128a
Embil22 <96885540+Embil22@users.noreply.github.com>
2026-04-29 11:37:55 +0200

    Update f1.sql

--- backend/f1.sql
+++ backend/f1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2026. Ápr 26. 23:35
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Létrehozás ideje: 2026. Ápr 29. 11:37
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,20 +37,6 @@ CREATE TABLE `quiz_attempts` (
   `correct_answers` int(11) NOT NULL,
   `completed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `quiz_attempts`
---
-
-INSERT INTO `quiz_attempts` (`id`, `user_id`, `score_percent`, `total_questions`, `correct_answers`, `completed_at`) VALUES
-(1, 1, 0, 24, 0, '2026-04-26 20:37:03'),
-(2, 1, 0, 24, 0, '2026-04-26 20:37:23'),
-(3, 1, 0, 24, 0, '2026-04-26 20:37:39'),
-(4, 1, 0, 24, 0, '2026-04-26 20:49:18'),
-(5, 1, 0, 24, 0, '2026-04-26 20:51:16'),
-(6, 2, 4, 24, 1, '2026-04-26 21:13:13'),
-(7, 2, 0, 24, 0, '2026-04-26 21:19:04'),
-(8, 3, 96, 24, 23, '2026-04-26 21:34:18');
 
 -- --------------------------------------------------------
 
@@ -111,15 +97,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- A tábla adatainak kiíratása `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `created_at`) VALUES
-(1, 'Embil22', 'balazsmartin2008@gmail.com', '$2y$10$hIhdwKlG5l48V/HFl8f4tubr/t4uRP4/gNXjLLkJY/ZVCtLgq4ssS', '2026-04-26 20:36:19'),
-(2, '123', 'balazsmartin20080@gmail.com', '$2y$10$m4CNkdXjcn1RQnG7x1OXLOeMxS8kQnV4MMloBttm.xuD7LglnMxky', '2026-04-26 20:56:19'),
-(3, 'admin', 'admin@gmail.com', '$2y$10$aK5G.bFAzm73zihVT5UIMuBX6/Lx.rzzNqevpW0OnjC7QP.uR5DLK', '2026-04-26 21:25:03');
-
---
 -- Indexek a kiírt táblákhoz
 --
 
@@ -152,7 +129,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `quiz_attempts`
 --
 ALTER TABLE `quiz_attempts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT a táblához `tracks`
@@ -164,7 +141,7 @@ ALTER TABLE `tracks`
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Megkötések a kiírt táblákhoz
